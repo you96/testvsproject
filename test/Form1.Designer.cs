f11c3f11ced2f358b13e814287ab8b891c50ad32
Jintao <Jintao>
2016-04-22 17:07:38 +0200

    chage color

--- test/Form1.Designer.cs
+++ test/Form1.Designer.cs
@@ -103,6 +103,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(284, 262);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.button4);
